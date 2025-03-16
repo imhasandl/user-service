6b1516f089145f19ce46f877472498060d37e263
Hasan <123514871+imhasandl@users.noreply.github.com>
2025-03-16 19:51:37 +0500

    new migration file

---
+++ cmd/helper/responseGRPC.go
@@ -0,0 +1,33 @@
+package helper
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func RespondWithErrorGRPC(ctx context.Context, code codes.Code, msg string, err error) error {
+	if err != nil {
+		log.Println(err)
+	}
+	
+	if code > codes.Internal { // 5XX equivalent in gRPC
+		log.Printf("Responding with 5XX gRPC error: %s", msg)
+	}
+
+	type errorResponse struct {
+		UserServiceError string `json:"error"`
+	}
+
+	jsonBytes, err := json.Marshal(errorResponse{UserServiceError: msg})
+	if err != nil {
+		log.Printf("Error marshalling error JSON: %s", err)
+		return status.Errorf(codes.Internal, "Failed to marshal error response")
+	}
+
+	log.Printf("UserServiceError: %s, Code: %s", string(jsonBytes), code.String()) // Log the error
+	return status.Errorf(code, msg)
+}
--- cmd/server/user-service.go
+++ cmd/server/user-service.go
@@ -8,9 +8,9 @@ import (
 	postService "github.com/imhasandl/post-service/cmd/helper"
 	"github.com/imhasandl/user-service/internal/database"
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
+	helper "github.com/imhasandl/user-service/cmd/helper"
 	pb "github.com/imhasandl/user-service/protos"
 )
 
@@ -39,7 +39,7 @@ func (s *server) GetUserByEmailOrUsername(
 
 	user, err := s.db.GetUserByEmailOrUsername(ctx, userParams)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "can't get user from db: %v - GetUserByEmailOrUsername", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.Internal, "can't get user from db: GetUserByEmailOrUsername", err)
 	}
 
 	return &pb.GetUserByEmailOrUsernameResponse{
@@ -57,12 +57,12 @@ func (s *server) GetUserByEmailOrUsername(
 func (s *server) GetUserByID(ctx context.Context, req *pb.GetUserByIDRequest) (*pb.GetUserByIDResponse, error) {
 	userID, err := uuid.Parse(req.GetId())
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "can't parse user id from incomin request: %v - GetUserByID", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.Internal, "can't parse user id from incoming request: GetUserByID", err)
 	}
 
 	user, err := s.db.GetUserById(ctx, userID)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "can't get user from db: %v - GetUserByID", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.Internal, "can't get user from db: GetUserByID", err)
 	}
 
 	return &pb.GetUserByIDResponse{
@@ -82,17 +82,17 @@ func (s *server) GetUserByID(ctx context.Context, req *pb.GetUserByIDRequest) (*
 func (s *server) GetUserByToken(ctx context.Context, req *pb.GetUserByTokenRequest) (*pb.GetUserByTokenResponse, error) {
 	accessToken, err := postService.GetBearerTokenFromGrpc(ctx)
 	if err != nil {
-		return nil, status.Errorf(codes.InvalidArgument, "can't get token from header: %v - GetUserByToken", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.InvalidArgument, "can't get token from header: GetUserByToken", err)
 	}
 
 	userID, err := postService.ValidateJWT(accessToken, s.tokenSecret)
 	if err != nil {
-		return nil, status.Errorf(codes.Unauthenticated, "can't validate token: %v - GetUserByToken", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.Unauthenticated, "can't validate token: GetUserByToken", err)
 	}
 
 	user, err := s.db.GetUserById(ctx, userID)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "can't get user from db: %v - GetUserByToken", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.Internal, "can't get user from db: GetUserByToken", err)
 	}
 
 	return &pb.GetUserByTokenResponse{
@@ -112,7 +112,7 @@ func (s *server) GetUserByToken(ctx context.Context, req *pb.GetUserByTokenReque
 func (s *server) GetAllUsers(ctx context.Context, req *pb.GetAllUsersRequest) (*pb.GetAllUsersResponse, error) {
 	users, err := s.db.GetAllUsers(ctx)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "can't get users from db: %v - GetAllUsers", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.Internal, "can't get users from db: GetAllUsers", err)
 	}
 
 	pbUsers := make([]*pb.User, len(users))
@@ -137,12 +137,12 @@ func (s *server) GetAllUsers(ctx context.Context, req *pb.GetAllUsersRequest) (*
 func (s *server) ChangeUsername(ctx context.Context, req *pb.ChangeUsernameRequest) (*pb.ChangeUsernameResponse, error) {
 	accessToken, err := postService.GetBearerTokenFromGrpc(ctx)
 	if err != nil {
-		return nil, status.Errorf(codes.InvalidArgument, "can't get token from header: %v - ChangeUsername", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.InvalidArgument, "can't get token from header: ChangeUsername", err)
 	}
 
 	userID, err := postService.ValidateJWT(accessToken, s.tokenSecret)
 	if err != nil {
-		return nil, status.Errorf(codes.Unauthenticated, "you are not allowed to change username: %v - ChangeUsername", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.Unauthenticated, "you are not allowed to change username: ChangeUsername", err)
 	}
 
 	changeUsernameParams := database.ChangeUsernameParams{
@@ -152,7 +152,7 @@ func (s *server) ChangeUsername(ctx context.Context, req *pb.ChangeUsernameReque
 
 	user, err := s.db.ChangeUsername(ctx, changeUsernameParams)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "can't change username in db: %v - ChangeUsername", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.Internal, "can't change username in db: ChangeUsername", err)
 	}
 
 	return &pb.ChangeUsernameResponse{
@@ -172,17 +172,17 @@ func (s *server) ChangeUsername(ctx context.Context, req *pb.ChangeUsernameReque
 func (s *server) ChangePassword(ctx context.Context, req *pb.ChangePasswordRequest) (*pb.ChangePasswordResponse, error) {
 	accessToken, err := postService.GetBearerTokenFromGrpc(ctx)
 	if err != nil {
-		return nil, status.Errorf(codes.InvalidArgument, "can't get authorization token from header: %v - ChangePassword", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.InvalidArgument, "can't get authorization token from header: ChangePassword", err)
 	}
 
 	userID, err := postService.ValidateJWT(accessToken, s.tokenSecret)
 	if err != nil {
-		return nil, status.Errorf(codes.Unauthenticated, "you are not allowed to delete user: %v - ChangePassword", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.Unauthenticated, "you are not allowed to delete user: ChangePassword", err)
 	}
 
 	hashedPassword, err := authService.HashPassword(req.GetPassword())
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "can't hash password: %v - ChangePassword", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.Internal, "can't hash password: ChangePassword", err)
 	}
 
 	changePasswordParams := database.ChangePasswordParams{
@@ -192,7 +192,7 @@ func (s *server) ChangePassword(ctx context.Context, req *pb.ChangePasswordReque
 
 	err = s.db.ChangePassword(ctx, changePasswordParams)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "can't change password: %v - ChangePassword", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.Internal, "can't change password: ChangePassword", err)
 	}
 
 	return &pb.ChangePasswordResponse{
@@ -203,29 +203,29 @@ func (s *server) ChangePassword(ctx context.Context, req *pb.ChangePasswordReque
 func (s *server) DeleteUser(ctx context.Context, req *pb.DeleteUserRequest) (*pb.DeleteUserResponse, error) {
 	accessToken, err := postService.GetBearerTokenFromGrpc(ctx)
 	if err != nil {
-		return nil, status.Errorf(codes.InvalidArgument, "can't get authorization token from header: %v - DeleteUser", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.InvalidArgument, "can't get authorization token from header: DeleteUser", err)
 	}
 
 	userID, err := postService.ValidateJWT(accessToken, s.tokenSecret)
 	if err != nil {
-		return nil, status.Errorf(codes.Unauthenticated, "you are not allowed to delete user: %v - DeleteUser", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.Unauthenticated, "you are not allowed to delete user: DeleteUser", err)
 	}
 
 	user, err := s.db.GetUserById(ctx, userID)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "can't get user from db: %v - DeleteUser", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.Internal, "can't get user from db: DeleteUser", err)
 	}
 
 	if err := authService.CheckPassword(user.Password, req.GetPassword()); err != nil {
-		return nil, status.Errorf(codes.InvalidArgument, "password is not correct: %v - DeleteUser", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.InvalidArgument, "password is not correct: DeleteUser", err)
 	}
 
 	if req.GetVerifyMessage() != "SUBMIT" {
-		return nil, status.Errorf(codes.InvalidArgument, "you must submit 'SUBMIT' to delete your account")
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.InvalidArgument, "you must submit 'SUBMIT' to delete your account", nil)
 	}
 
 	if err = s.db.DeleteUser(ctx, userID); err != nil {
-		return nil, status.Errorf(codes.Internal, "can't delete user from db: %v - DeleteUser", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.Internal, "can't delete user from db: DeleteUser", err)
 	}
 
 	return &pb.DeleteUserResponse{
@@ -234,43 +234,43 @@ func (s *server) DeleteUser(ctx context.Context, req *pb.DeleteUserRequest) (*pb
 }
 
 func (s *server) SendVerificationCode(
-	ctx context.Context, 
+	ctx context.Context,
 	req *pb.SendVerificationCodeRequest,
-	) (*pb.SendVerificationCodeResponse, error) {
+) (*pb.SendVerificationCodeResponse, error) {
 
 	accessToken, err := postService.GetBearerTokenFromGrpc(ctx)
 	if err != nil {
-		return nil, status.Errorf(codes.InvalidArgument, "can't get authorization token from header: %v - Reset", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.InvalidArgument, "can't get authorization token from header: SendVerificationCode", err)
 	}
 
 	userID, err := postService.ValidateJWT(accessToken, s.tokenSecret)
 	if err != nil {
-		return nil, status.Errorf(codes.Unauthenticated, "you are not allowed to reset password: %v - ResetPassword", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.Unauthenticated, "you are not allowed to reset password: SendVerificationCode", err)
 	}
 
 	user, err := s.db.GetUserById(ctx, userID)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "can't get user from db: %v - ResetPassword", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.Internal, "can't get user from db: SendVerificationCode", err)
 	}
 
 	verificationCode, err := authService.GenerateVerificationCode()
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "can't generate verification code: %v - ResetPassword", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.Internal, "can't generate verification code: SendVerificationCode", err)
 	}
 
 	sendResetVerificationCodeParams := database.SendResetVerificationCodeParams{
-		ID: userID,
+		ID:               userID,
 		VerificationCode: verificationCode,
 	}
 
 	err = s.db.SendResetVerificationCode(ctx, sendResetVerificationCodeParams)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "can't send verification code: %v - ResetPassword", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.Internal, "can't send verification code: SendVerificationCode", err)
 	}
 
 	err = authService.SendVerificationEmail(user.Email, s.emailSecret, verificationCode)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "can't send verification email: %v - ResetPassword", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.Internal, "can't send verification email: SendVerificationCode", err)
 	}
 
 	return &pb.SendVerificationCodeResponse{
@@ -281,41 +281,41 @@ func (s *server) SendVerificationCode(
 func (s *server) ResetPassword(ctx context.Context, req *pb.ResetPasswordRequest) (*pb.ResetPasswordResponse, error) {
 	accessToken, err := postService.GetBearerTokenFromGrpc(ctx)
 	if err != nil {
-		return nil, status.Errorf(codes.InvalidArgument, "can't get authorization token from header: %v - ResetPassword", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.InvalidArgument, "can't get authorization token from header: ResetPassword", err)
 	}
 
 	userID, err := postService.ValidateJWT(accessToken, s.tokenSecret)
 	if err != nil {
-		return nil, status.Errorf(codes.Unauthenticated, "you are not allowed to reset password: %v - ResetPassword", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.Unauthenticated, "you are not allowed to reset password: ResetPassword", err)
 	}
 
 	user, err := s.db.GetUserById(ctx, userID)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "can't get user from db: %v - ResetPassword", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.Internal, "can't get user from db: ResetPassword", err)
 	}
 
 	if req.GetVerificationCode() != user.VerificationCode {
-		return nil, status.Errorf(codes.InvalidArgument, "verification code is not correct")
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.InvalidArgument, "verification code is not correct", nil)
 	}
 
 	err = s.db.VerifyVerificationCode(ctx, userID)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "can't verify verification code: %v - ResetPassword", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.Internal, "can't verify verification code: ResetPassword", err)
 	}
 
 	newPassword, err := authService.HashPassword(req.NewPassword)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "can't hash new password: %v - ResetPassword", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.Internal, "can't hash new password: ResetPassword", err)
 	}
 
 	resetPasswordParams := database.ResetPasswordParams{
-		ID: userID,
+		ID:       userID,
 		Password: newPassword,
 	}
 
 	err = s.db.ResetPassword(ctx, resetPasswordParams)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "can't reset password: %v - ResetPassword", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.Internal, "can't reset password: ResetPassword", err)
 	}
 
 	return &pb.ResetPasswordResponse{
@@ -326,7 +326,7 @@ func (s *server) ResetPassword(ctx context.Context, req *pb.ResetPasswordRequest
 func (s *server) DeleteAllUsers(ctx context.Context, req *pb.DeleteAllUsersRequest) (*pb.DeleteAllUsersResponse, error) {
 	err := s.db.DeleteAllUsers(ctx)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "can't delete user from db: %v - DeleteUser", err)
+		return nil, helper.RespondWithErrorGRPC(ctx, codes.Internal, "can't delete user from db: DeleteAllUsers", err)
 	}
 
 	return &pb.DeleteAllUsersResponse{
